d101d538f9c485bdc4921d88f3b3ab5c5ad0ae75
Derekii3XD <136671444+Derekii3XD@users.noreply.github.com>
2025-07-03 13:23:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ server.use(router);
 
 
 // Iniciar el servidor en el puerto 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
